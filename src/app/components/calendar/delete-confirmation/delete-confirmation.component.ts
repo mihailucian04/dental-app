8b4e94b20abc265d6aeb6ceba03b2cb4969850d2
Mihai Boian <mboian@isoftwaresystems.com>
2019-08-19 02:28:39 +0300

    feat(calendar): add delete confirmation

---
+++ src/app/components/calendar/delete-confirmation/delete-confirmation.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, Inject, NgZone } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+import { SnackBarService } from 'src/app/services/snack-bar.service';
+import { GoogleDataService } from 'src/app/services/google-data.service';
+
+@Component({
+  selector: 'app-delete-confirmation',
+  templateUrl: './delete-confirmation.component.html',
+  styleUrls: ['./delete-confirmation.component.scss']
+})
+export class DeleteConfirmationComponent {
+
+  constructor(
+    public dialogRef: MatDialogRef<DeleteConfirmationComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private googleDataService: GoogleDataService,
+    private ngZone: NgZone,
+    private snackBarService: SnackBarService) { }
+
+    public deleteAppointment() {
+      this.ngZone.runOutsideAngular(() => {
+        this.googleDataService.deleteCalendarEvent(this.data.id).then(() => {
+          this.ngZone.run(() => {
+            this.dialogRef.close();
+            this.snackBarService.show('Appointemnt successfully deleted!');
+          });
+        });
+      });
+    }
+
+    public closeDialog() {
+      this.dialogRef.close();
+    }
+}
